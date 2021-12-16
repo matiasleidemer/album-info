89073315a891eee1a2ab3610456c2216eed7f16a
Matias Leidemer <matiasleidemer@gmail.com>
2021-12-16 11:27:00 -0300

    Add album and landing pages

--- app/routes/$albumId.tsx
+++ app/routes/$albumId.tsx
@@ -1,36 +1,83 @@
-import { useLoaderData, json, Link } from "remix";
-import { getCredentials, getAccessToken } from "~/lib/spotify.server";
-
+import invariant from "tiny-invariant";
+import { useLoaderData } from "remix";
 import type { MetaFunction, LoaderFunction } from "remix";
 
-type Album = {
-  resources: Array<{ name: string; url: string }>;
-  demos: Array<{ name: string; to: string }>;
+import { getCredentials, getAccessToken, getAlbum } from "~/lib/spotify.server";
+
+export const loader: LoaderFunction = async ({ params: { albumId } }) => {
+  invariant(typeof albumId === "string", "invalid album id");
+
+  const [clientId, clientSecret] = getCredentials();
+
+  const accessToken = await getAccessToken(clientId, clientSecret);
+  const album = await getAlbum({ albumId, accessToken });
+
+  return album;
 };
 
-export let loader: LoaderFunction = async ({ params: { albumId } }) => {
-  // const [clientId, clientSecret] = getCredentials();
-  // const token = await getAccessToken(clientId, clientSecret);
-
-  // console.log(token);
-  // console.log({ albumId });
-
-  return null;
-};
-
-export let meta: MetaFunction = () => {
+export const meta: MetaFunction = ({ data }) => {
   return {
-    title: "Album Info",
-    description: "Welcome to remix!",
+    title: `${data.name} by ${data.artists[0].name}`,
+    description: `Get to know ${data.name} a little bit better`,
+    "twitter:image": data.images[0].url,
+    "twitter:card": "summary_large_image",
   };
 };
 
+const formatLength = (ms: number) => {
+  const date = new Date(ms);
+  const minutes = `0${date.getMinutes()}`.slice(-2);
+  const seconds = `0${date.getSeconds()}`.slice(-2);
+
+  return `${minutes}:${seconds}`;
+};
+
 export default function Index() {
-  let data = useLoaderData<Album>();
+  let data = useLoaderData();
+
+  const cover = data.images[0];
+  const artist = data.artists[0];
 
   return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-5xl text-center font-extrabold">Album Info</h1>
-    </main>
+    <>
+      <header className="mb-16">
+        <h1 className="text-5xl text-center font-extrabold">Album Info</h1>
+      </header>
+
+      <main className="container mx-auto p-4">
+        <div className="flex flex-col gap-4 md:flex-row">
+          <div className="flex-1">
+            <img
+              src={cover.url}
+              width={cover.width}
+              height={cover.height}
+              className="mb-4"
+            />
+            <p>
+              {data.name} <span>by</span> {artist.name}
+            </p>
+            <p>
+              Released on {data.release_date} via {data.label}
+            </p>
+            <p>
+              Spotify URL:{" "}
+              <a href={data.href} target="_blank">
+                {data.href}
+              </a>
+            </p>
+          </div>
+          <div className="flex-1">
+            <h2 className="mb-4">Tracklist</h2>
+            <ul>
+              {data.tracks.items.map((track) => (
+                <li>
+                  {track.name} ({formatLength(track.duration_ms)})
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,31 +6,53 @@ import type { MetaFunction, ActionFunction, LoaderFunction } from "remix";
 
 export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData();
-  const albumId = form.get("albumId");
+  const albumUrl = form.get("albumUrl");
 
-  invariant(typeof albumId === "string", "invalid album id");
+  invariant(typeof albumUrl === "string", "invalid album id");
 
-  return redirect(`/${albumId}`);
+  const url = new URL(albumUrl).pathname.split("/").slice(-1)[0];
+
+  return redirect(`/${url}`);
 };
 
 export const meta: MetaFunction = () => {
   return {
     title: "Album Info",
-    description: "Welcome to remix!",
+    description: "Get to know your albums better",
   };
 };
 
 export default function Index() {
   return (
     <main className="container mx-auto p-4">
-      <h1 className="text-5xl text-center font-extrabold">Album Info</h1>
+      <h1 className="text-5xl text-center font-extrabold mb-16">Album Info</h1>
 
-      <Form method="post">
-        <label htmlFor="albumId">Album ID</label>
-        <input type="text" name="albumId" id="albumId" className="border-2" />
-
-        <button type="submit">Search</button>
-      </Form>
+      <div className="flex justify-center">
+        <Form method="post" className="w-2/4">
+          <fieldset className="mb-8">
+            <label htmlFor="albumUrl" className="font-bold">
+              Spotify album URL:
+            </label>
+            <p>
+              <input
+                type="text"
+                name="albumUrl"
+                id="albumUrl"
+                placeholder="https://open.spotify.com/album/4fW7CblaSvdhPWmcCFgZbQ?si=CtA_eAGETxWbjcnSjyIDTA"
+                className="placeholder:italic placeholder:text-gray-400 block w-full border border-gray-300 rounded-md py-2 px-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              />
+            </p>
+          </fieldset>
+          <div>
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Search
+            </button>
+          </div>
+        </Form>
+      </div>
     </main>
   );
 }
